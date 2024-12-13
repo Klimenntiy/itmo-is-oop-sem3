b12e435a16e11ec7a3329b9be0baa4b660ed923f
Klimenntiy <animilk04@gmail.com>
2024-12-14 02:21:17 +0300

    add lab-5 all (pls)

---
+++ src/Lab5.Presentation.Console/Scenarios/Creating/CreateAccountScenarioProvider.cs
@@ -0,0 +1,31 @@
+using Lab5.Application.Contracts.Users;
+using Lab5.Application.Models.Users;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Lab5.Presentation.Console.Scenarios.Creating;
+
+public class CreateAccountScenarioProvider : IScenarioProvider
+{
+    private readonly IAdminService _adminService;
+    private readonly ICurrentUserService _currentUser;
+
+    public CreateAccountScenarioProvider(
+        IAdminService adminService, ICurrentUserService currentUser)
+    {
+        _adminService = adminService;
+        _currentUser = currentUser;
+    }
+
+    public bool TryGetScenario(
+        [NotNullWhen(true)] out IScenario? scenario)
+    {
+        if (_currentUser.User is null || _currentUser.User.Role is not UserRole.Admin)
+        {
+            scenario = null;
+            return false;
+        }
+
+        scenario = new CreateAccountScenario(_adminService);
+        return true;
+    }
+}
---
+++ src/Lab5.Presentation.Console/Scenarios/Login/CustomerLoginScenarioProvider.cs
@@ -0,0 +1,31 @@
+using Lab5.Application.Contracts.Users;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Lab5.Presentation.Console.Scenarios.Login;
+
+public class CustomerLoginScenarioProvider : IScenarioProvider
+{
+    private readonly IUserService _userService;
+    private readonly ICurrentUserService _currentUser;
+
+    public CustomerLoginScenarioProvider(
+        IUserService userService,
+        ICurrentUserService currentUser)
+    {
+        _userService = userService;
+        _currentUser = currentUser;
+    }
+
+    public bool TryGetScenario(
+        [NotNullWhen(true)] out IScenario? scenario)
+    {
+        if (_currentUser.User is not null)
+        {
+            scenario = null;
+            return false;
+        }
+
+        scenario = new CustomerLoginScenario(_userService);
+        return true;
+    }
+}
